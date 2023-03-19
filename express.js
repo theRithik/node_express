2e4ebb69e941303c9af9e8849890dda5dc19b18b
rithik <115656594+theRithik@users.noreply.github.com>
2023-03-19 21:15:29 +0530

    Update express.js

--- express.js
+++ express.js
@@ -5,4 +5,11 @@ app.get('/', (req, res)=>{
     res.send('hello world')
 })
 
+app.get('/getmovie',(req, res)=>{
+    fs.readFile('./data/movies.json', 'utf8', (err,data)=>{
+        if(err){
+            console.log(err)
+        }
+      
+
 app.listen(3000)
